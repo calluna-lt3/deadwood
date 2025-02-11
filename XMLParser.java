e6510ac9443c3898b193573d6782174dcf33c7e9
Calluna <106458302+CallunaLessThan3@user.noreply.github.com>
2025-02-10 18:53:16 -0800

    Added parser and controller classes, moved data to own folder

---
+++ XMLParser.java
@@ -0,0 +1,10 @@
+public class XMLParser {
+    private final String boardPath = "./data/board";
+    private final String cardsPath = "./data/cards";
+
+
+    public void parse() {}
+
+    private void parseBoard() {}
+    private void parseCards() {}
+}
