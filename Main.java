d6ed8c6f551520e4d16916905d0c59923e61e43e
Calluna <106458302+CallunaLessThan3@user.noreply.github.com>
2025-02-12 15:07:33 -0800

    add testing for xmlparser

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         XMLParser xmlp = new XMLParser();
-        /*
         SceneCard[] cards = xmlp.getSceneCards();
         for (SceneCard card : cards) {
             System.out.println("name: " + card.name);
@@ -18,32 +18,25 @@ public class Main {
                 System.out.println("");
             }
         }
-        */
 
-        Room[] rooms = xmlp.getRooms();
+        ArrayList<Room> rooms = xmlp.getRooms();
         for (Room room : rooms) {
-            if (room == null) continue;
             SoundStage ss = (SoundStage)room;
             System.out.println("name: " + ss.name);
-            /*
             System.out.println("  shotMarkers: " + ss.shotMarkers);
 
             System.out.println("  Adjacent Rooms: ");
             for (String adjRoom : ss.adjacentRoomsStr) {
-                if (adjRoom == null) continue;
-                System.out.println(adjRoom);
+                System.out.println("    " + adjRoom);
             }
 
             System.out.println("  roles: ");
             for (Role role : ss.roles) {
-                if (role == null) continue;
                 System.out.println("    name: " + role.name);
                 System.out.println("    line: " + role.line);
                 System.out.println("    rank: " + role.rank);
                 System.out.println("");
             }
-            */
-
         }
     }
 }
